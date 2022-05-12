32dbbf9df3b9f323aba7034350f115d66deeb475
ThomasDickson <thomas.dickson13@outlook.com>
2022-05-13 00:29:21 +1000

    commit

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from './Button'
 
 function Video() {
   return (
@@ -6,6 +7,9 @@ function Video() {
       <video src="/planevideo.mp4" autoPlay loop />
       <h1>Come fly with us.</h1>
       <p>For quick cheap trips and more!</p>
+      <div className='hero-btns'>
+
+      </div>
     </div>
   )
 }
