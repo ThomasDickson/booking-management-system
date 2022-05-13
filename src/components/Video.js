68c6fd83d0159b791b2e96d08afce4f164fb86a4
ThomasDickson <thomas.dickson13@outlook.com>
2022-05-14 04:42:10 +1000

    hero section progress

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import { Box } from '@mui/material';
 import "../App.css";
 import "./Video.css";
 
 function Video() {
+
   return (
-    <div>
-      <video src="/planevideo.mp4" autoPlay loop muted/>
-      <p className="bigText">Come fly with us.</p>
-      <p className="smallText">For quick cheap trips and more!</p>
-    </div>
+    <Box className="video-container"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      marginTop="100px"
+      width={'100%'}
+      height={'300px'}>
+      <video src="/planevideo.mp4" autoPlay loop muted/> 
+
+      <Box className="text-container"
+        display="flex"
+        justifyContent="flex-start"
+        alignItems="center"
+        marginTop="100px"
+        marginLeft="20%"
+        width={'100%'}
+        height={'300px'}
+      >
+        <p className="bigText">Come fly with us.</p>
+        <p className="smallText">For quick cheap trips and more!</p>
+      </Box>
+             
+    </Box>
   )
 }
 
